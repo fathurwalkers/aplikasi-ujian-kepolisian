ebf0972484b1a0df962b976dedbd629c2203e81d
Fathur Walkers <fathurwalkers44@gmail.com>
2022-07-19 22:26:48 -0700

    update

--- resources/views/ukom/data-ukom.blade.php
+++ resources/views/ukom/data-ukom.blade.php
@@ -81,12 +81,53 @@
                                     <td>{{ $item->ukom_nama }}</td>
                                     <td>{{ $item->ukom_kode }}</td>
                                     <td class="d-flex justify-content-center mx-auto">
-                                        <button type="button" class="btn btn-sm btn-success mr-1">Lihat</button>
-                                        <button type="button" class="btn btn-sm btn-info mr-1">Ubah</button>
-                                        <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#modalhapus">Hapus</button>
 
+                                        <div class="row btn-group">
+                                            <div class="col-sm-12 col-md-12 col-lg-12 btn-group">
+                                                <button type="button" class="btn btn-sm btn-success mr-1">Lihat</button>
+                                                <button type="button" class="btn btn-sm btn-info mr-1" data-toggle="modal" data-target="#modalupdate">Ubah</button>
+                                                <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#modalhapus">Hapus</button>
+                                            </div>
+                                        </div>
+
+                                        {{-- MODAL UPDATE DATA UKOM --}}
+                                        <div class="modal fade" id="modalupdate" tabindex="-1" role="dialog"
+                                        aria-labelledby="exampleModalLabelLogout" aria-hidden="true">
+                                            <div class="modal-dialog" role="document">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="exampleModalLabelLogout">Form Ubah Data UKOM</h5>
+                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                            <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>
+                                                    <form action="{{ route('post-update-ukom') }}" method="POST">
+                                                        @csrf
+                                                        <div class="modal-body">
+
+                                                            <div class="row">
+                                                                <div class="col-sm-12 col-md-12 col-lg-12">
+                                                                    <div class="form-group">
+                                                                        <label for="ukom_nama">Uji Kompetensi</label>
+                                                                        <input type="text" class="form-control" id="ukom_nama" placeholder="Contoh : Bahasa Indonesia" name="ukom_nama">
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-outline-primary"
+                                                                data-dismiss="modal">Batalkan</button>
+                                                            <button type="submit" class="btn btn-primary">Simpan</button>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        {{-- MODAL HAPUS UKOM --}}
                                         <div class="modal fade" id="modalhapus" tabindex="-1" role="dialog"
-                                            aria-labelledby="exampleModalLabelLogout" aria-hidden="true">
+                                        aria-labelledby="exampleModalLabelLogout" aria-hidden="true">
                                             <div class="modal-dialog" role="document">
                                                 <div class="modal-content">
                                                     <div class="modal-header">
